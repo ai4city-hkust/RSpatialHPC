ce61eb82fb1298cd14c35887ea87bf1cf263bb1a
SpatLyu <lyu.geosocial@gmail.com>
2025-02-17 19:43:59 +0800

    add r demo script

---
+++ script/spEDM_PreNPP.R
@@ -0,0 +1,23 @@
+library(spEDM)
+
+npp = terra::rast(system.file("extdata/npp.tif", package = "spEDM"))
+
+pred = as.matrix(merge(seq(10,terra::nrow(npp),10), seq(10,terra::ncol(npp),10)))
+
+startTime = Sys.time()
+npp_res = gccm(data = npp,
+               cause = "pre",
+               effect = "npp",
+               libsizes = seq(20,300,20),
+               E = 2,
+               k = 5,
+               pred = pred,
+               progressbar = TRUE)
+endTime = Sys.time()
+print(difftime(endTime,startTime, units ="mins"))
+
+npp_res
+
+plot(npp_res)
+
+
